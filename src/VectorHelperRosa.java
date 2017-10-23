2bb4dd31d08b34c8da7e3bd808d147c861f40056
rosa <fr_khenniche@esi.dz>
2017-10-23 10:40:41 +0100

    modified master

--- src/VectorHelperRosa.java
+++ src/VectorHelperRosa.java
@@ -11,7 +11,7 @@ public class VectorHelperRosa {
      **/
         for (int i=1; i<v.length; i++)
         {
-            if (v[i]>MaxMin[0]) MaxMin[0]=v[i];
+            if (v[i]>MaxMin[0]) MaxMin[0]=v[i]; /* commentaire branche master*/
             if (v[i]<MaxMin[1]) MaxMin[1]=v[i];
         }
         return MaxMin ;
